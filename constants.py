c1dd13552095d85cc321d79464f46dbdd28d0481
Jonathan Grizou <jonathan.grizou@gmail.com>
2018-01-03 16:30:58 +0000

    relative path to dropfactory_exploration

--- constants.py
+++ constants.py
@@ -1,4 +1,15 @@
-DROPFACTORY_UTILS_PATH = '/home/group/workspace/dropfactory_exploration/utils/'
+import os
+
+# this get our current location in the file system
+import inspect
+HERE_PATH = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+
+# adding parent directory to path, so we can access the utils easily
+import sys
+root_path = os.path.join(HERE_PATH, '..')
+sys.path.append(root_path)
+
+DROPFACTORY_UTILS_PATH = os.path.join(HERE_PATH, '..', 'dropfactory_exploration', 'utils')
 
 ORKNEY_XP_FOLDER = '/home/group/orkney1/Chemobot/dropfactory_exploration/realworld_experiments/'
 
